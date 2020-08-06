28cd7b4c2eec0e050e76660536137b31bf82fb5b
Alexa Kaminsky <akaminsky9@gmail.com>
2020-08-06 23:08:22 +0200

    filters

--- script/script.js
+++ script/script.js
@@ -3,3 +3,22 @@ $('.resource-category-wrapper').on("click", function () {
 	$(this).find(".resource-category").toggleClass('category-active');
 	$(this).toggleClass('auto-height');
 });
+
+
+$('.toggle-labels').on("click", function () {
+	$(".resource-category").toggleClass('category-active');
+	$('.resource-category-wrapper').toggleClass('auto-height');
+});
+
+
+
+$('.resource-checkboxes').change(function() {
+
+   var myID = $(this).attr("id");
+
+
+
+		$('.resource.'+myID).toggle();
+
+
+});
