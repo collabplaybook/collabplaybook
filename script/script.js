78e738e8a4cd21a3a55701d81b8fc46976691fc9
Alexa Kaminsky <akaminsky9@gmail.com>
2020-12-29 23:43:25 +0100

    change styles of categories

--- script/script.js
+++ script/script.js
@@ -1,25 +1,10 @@
 
-$('.resource-category-wrapper').on("click", function () {
-	$(this).find(".resource-category").toggleClass('category-active');
-	$(this).toggleClass('auto-height');
-});
-
-
-$('.toggle-labels').on("click", function () {
-	$(".resource-category").toggleClass('category-active');
-	$('.resource-category-wrapper').toggleClass('auto-height');
-});
-
 
 
 $('.resource-checkboxes').change(function() {
 
    var myID = $(this).attr("id");
-
-
-
 		$('.resource.'+myID).toggle();
-
 
 });
 
